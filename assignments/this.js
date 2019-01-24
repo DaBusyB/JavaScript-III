0b000b3000789e9997447da6db5336cb538246c4
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-01-24 11:56:51 -0500

    work on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,37 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window - everything there is, no specificity
+* 2. implicit - binding that happens automatically
+* 3. new - constructor function thats created to duplicate objects
+* 4. explicit - binding that that is forced
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
+function hello() {
+    console.log(this)
+}
+
+hello();
 
 // Principle 2
-
 // code example for Implicit Binding
+const person = {
+    name: "Brandi",
+    age: 36,
+    fun: function() {
+        console.log(`${this.name} is ${this.age} years old`)
+    }
+}
+
+person.fun();
 
 // Principle 3
-
 // code example for New Binding
 
-// Principle 4
 
+// Principle 4
 // code example for Explicit Binding
