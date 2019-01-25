9dec8f2e7679a4f7b7faa2caf4015207c3f3ee3a
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-01-24 20:33:38 -0500

    practice with call and apply

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,8 @@
 *
 * 1. window - everything there is, no specificity
 * 2. implicit - binding that happens automatically
-* 3. new - constructor function thats created to duplicate objects
-* 4. explicit - binding that that is forced
+* 3. new - constructor function thats used to easily duplicate objects
+* 4. explicit - binding that that is forced/controlled
 *
 * write out a code example of each explanation above
 */
@@ -12,7 +12,7 @@
 // Principle 1
 // code example for Window Binding
 function hello() {
-    console.log(this)
+    console.log("Principle 1:", this)
 }
 
 hello();
@@ -23,7 +23,7 @@ const person = {
     name: "Brandi",
     age: 36,
     fun: function() {
-        console.log(`${this.name} is ${this.age} years old`)
+        console.log("Principle 2:", `${this.name} is ${this.age} years old`);
     }
 }
 
@@ -31,7 +31,43 @@ person.fun();
 
 // Principle 3
 // code example for New Binding
+function TeenTitan(member) {
+    this.member = member;
+    this.greeting = "Hey!";
+    this.speak = function() {
+        console.log("Principle 3a:", `${this.greeting}, ${this.member}`)
+    }
+}
 
+// const robin = {
+//     name: "Robin"
+// }
+
+const cyborg = new TeenTitan("Cyborg");
+
+console.log("Principle 3b:", cyborg)
+cyborg.speak();
 
 // Principle 4
 // code example for Explicit Binding
+const child = {
+    name: "Sonnet"
+}
+
+const skills = ['cooking', ' programming', ' comedy'];
+const pets = ['ferret', 'pigeon', 'rabbit'];
+     //using call
+function facts1(skills) {
+    console.log("Principle 4 call:", `Hello, my name is ${this.name}. I like: ${skills}.`);
+};
+
+facts1.call(child, skills);
+
+
+     //using apply
+function facts2(pet1,pet2, pet3) {
+   
+    console.log("Principle 4 apply:", `Hello, my name is ${this.name}. I have a: ${pet1}, ${pet2}, and ${pet3}.`);
+};
+
+facts2.apply(child, pets);
